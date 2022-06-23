72ec93afcfa8dd9e97dfdb90982d99f0f9276be3
aaknox <azhya.knox@gmail.com>
2022-06-23 09:13:33 -0500

    final commit

--- src/main/java/com/revature/eval/java/core/Order.java
+++ src/main/java/com/revature/eval/java/core/Order.java
@@ -5,6 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This custom annotation was made to simulate JUnit 5's @Order annotation and 
+ * allows the developers control of execution of these tests cases in EvaluationService.
+ * 
+ * This is then controlled by the OrdererRunner to manage test case execution by order number.
+ * @author Azhya Knox
+ **/
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 public @interface Order {
